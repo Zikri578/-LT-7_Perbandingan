d2efa0d93ed9387bc68ab172d8f3835298aaae11
MuhammadZikri <zikrim577@gmail.com>
2022-05-09 11:44:56 +0700

    Perbandingan

---
+++ perbandingan.js
@@ -0,0 +1,23 @@
+document.writeln("Operator Perbandingan yang diisi secara langsung untuk menampilkan output file secara terpisah");
+
+let result = 101.345 > 103.43;
+document.writeln("<li>" + result + "</li>");
+
+result = 092385 < 901123;
+document.writeln("<li>" + result + "</li>");
+
+result = 900925.14 >= 02374092;
+document.writeln("<li>" + result + "</li>");
+
+result = 3252305 <= -75092357;
+document.writeln("<li>" + result + "</li>");
+
+result = 'Saya' == "saya" ;
+document.writeln("<li>" + result + "</li>");
+
+result = 200 != '500';
+document.writeln("<li>" + result + "</li>");
+
+result = 1000 !== '1000';
+document.writeln("<li>" + result + "</li>");
+
